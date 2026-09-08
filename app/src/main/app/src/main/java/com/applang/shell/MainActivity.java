31e3dc5a17e46fdae685eb39302d7ee7c1554190
overvortex <gmohab887@gmail.com>
2026-09-08 03:30:35 +0300

    Create MainActivity.java

---
+++ app/src/main/app/src/main/java/com/applang/shell/MainActivity.java
@@ -0,0 +1,12 @@
+package com.applang.shell;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Future AppLang code loader will read assets/main.al here
+    }
+}
+
